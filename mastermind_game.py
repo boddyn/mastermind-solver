63a97af20dbee71fc25dfb7574b3a8137d1d0b53
Boddy <boddyn@msoe.edu>
2016-03-15 16:24:27 -0500

    MastermindGame init

--- mastermind_game.py
+++ mastermind_game.py
@@ -1,7 +1,22 @@
-"""
-Created on Tue Mar 15 15:17:46 2016
+import random
+class MastermindGame:
 
-@author: boddyn
-"""
-strings = ['Heyo', 'Ahi', 'Hello World', 'Hi Frau Nagel']
-print strings[0]
+    solution = [0]
+    guesses = [[0]]
+
+    def __init__(self, numPeople, numTents):
+        self.numPeople = numPeople
+        self.numTents = numTents
+        MastermindGame.solution = [numTents]
+        MastermindGame.solution = [i for i in range(0, numPeople)]
+        random.shuffle(MastermindGame.solution)
+        MastermindGame.solution = MastermindGame.solution[:numTents]
+        
+    def solution(self):
+        return MastermindGame.solution
+        
+def main():
+    x = MastermindGame(6, 4)
+    print x.solution
+    
+main()
